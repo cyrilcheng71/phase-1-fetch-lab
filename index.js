51852168b58a7a2afa78446a88d46a289ac26bec
Man Cheng <cyrilcheng@protonmail.com>
2022-09-14 15:04:36 -0700

    commit tree 67522dd21eb9107aa892c8f15de0ae1d2a050262

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then (res => res.json())
+  .then (json=> renderBooks(json));
 }
+  
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +18,54 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+
+
+
+
+
+
+
+// //DOM Render function
+// function renderOneAnimal(animal){
+//   //build Animal
+//   let card = document.createElement("li")
+//   card.className = "card"
+//   card.innerHTML=` 
+//     <img src ="${animal.image.Url}>
+//     <div class= "content">
+//       <h4>${animla.name}</h4>
+//       <p>
+//         $<span class="donation-count">${animal.donations}</span> Donated
+
+//       <p>
+//       <p>${animal.description}</p>
+//     </div>
+//     <div class="buttons">
+//       <button> Donated $10 <button>
+//       <button> set Fee </button>
+    
+//     `
+  
+// }
+
+// //add animal card to DOM
+// document.querySelector("anmial-list".appendChild(card))
+
+
+// //Fetch 
+// function getAllAnimals(){
+//   fetch("http:/localhost:3000/animalData")
+//   .then(res => res.json())
+//   .then(data => animalData.forEach(animal => renderOneAnimal(animal)))
+// }
+
+
+// //intiial Render
+// //Get data and redmer our animals to the dom
+// function intiialize(){
+//   getAllAnimals()
+
+// //   animalData.forEach(animal => renderOneAnimal(animal))
+//  }
